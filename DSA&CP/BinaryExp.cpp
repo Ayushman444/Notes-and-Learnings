f3cc79ab663fe0eab94da3b6df85d527aa985950
Ayushman Tiwari <ayushman5022@gmail.com>
2024-02-17 17:11:44 +0530

    Iterative Binary Exponentiation

---
+++ DSA&CP/BinaryExp.cpp
@@ -0,0 +1,13 @@
+int func(int a, int b){
+    int ans = 1;
+    while(b>0){
+        if(b&1){
+            ans=ans*a;
+            b-=1;
+        }else{
+            a = a*a;
+            b/=2;
+        }
+    }
+
+}
